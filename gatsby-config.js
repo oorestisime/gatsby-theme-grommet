854ad4419235ba7b72a445f3a1587d4442e7a896
Orestis Ioannou <o.ioannou@sysdream.com>
2019-01-01 13:36:15 +0100

    Rename everything to jsx

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,37 +1,39 @@
 require('dotenv').config();
 const data = require('./src/config/data');
 
-extraPlugins = [];
+const extraPlugins = [];
 
-process.env.GITHUB && extraPlugins.push({
-  resolve: 'gatsby-source-graphql',
-  options: {
-    typeName: 'GitHub',
-    fieldName: 'github',
-    // Url to query from
-    url: 'https://api.github.com/graphql',
-    // HTTP headers
-    headers: {
-      Authorization: `bearer ${process.env.GITHUB}`,
+if (process.env.GITHUB) {
+  extraPlugins.push({
+    resolve: 'gatsby-source-graphql',
+    options: {
+      typeName: 'GitHub',
+      fieldName: 'github',
+      // Url to query from
+      url: 'https://api.github.com/graphql',
+      // HTTP headers
+      headers: {
+        Authorization: `bearer ${process.env.GITHUB}`,
+      },
+      // Additional options to pass to node-fetch
+      fetchOptions: {},
     },
-    // Additional options to pass to node-fetch
-    fetchOptions: {},
-  },
-})
+  });
+}
 
-data.analytics.gaId && extraPlugins.push({
-  resolve: 'gatsby-plugin-google-analytics',
-  options: {
-    trackingId: data.analytics.gaId,
-    anonymize: true,
-    respectDNT: true,
-    cookieDomain: data.analytics.domain,
-  },
-}, );
+if (data.analytics.gaId) {
+  extraPlugins.push({
+    resolve: 'gatsby-plugin-google-analytics',
+    options: {
+      trackingId: data.analytics.gaId,
+      anonymize: true,
+      respectDNT: true,
+      cookieDomain: data.analytics.domain,
+    },
+  });
+}
 
-process.env.IG_ACCESS_TOKEN &&
-  data.instagram.username &&
-  data.instagram.instagram_id &&
+if (process.env.IG_ACCESS_TOKEN && data.instagram.username && data.instagram.instagram_id) {
   extraPlugins.push({
     resolve: 'gatsby-source-instagram',
     options: {
@@ -40,15 +42,16 @@ process.env.IG_ACCESS_TOKEN &&
       instagram_id: config.instagram_id,
     },
   });
+}
 
-data.instagram.username &&
-  !process.env.IG_ACCESS_TOKEN &&
+if (data.instagram.username && !process.env.IG_ACCESS_TOKEN) {
   extraPlugins.push({
-    resolve: `gatsby-source-instagram`,
+    resolve: 'gatsby-source-instagram',
     options: {
       username: data.instagram.username,
     },
   });
+}
 
 module.exports = {
   siteMetadata: {
@@ -78,7 +81,7 @@ module.exports = {
             resolve: 'gatsby-remark-rehype-images',
             options: {
               tag: 'rehype-image',
-              quality: 100
+              quality: 100,
             },
           },
           'gatsby-remark-prismjs',
